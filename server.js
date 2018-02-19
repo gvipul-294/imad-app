e21fc9744808163cbf28595226e595dc0d444513
Vipul Gupta <gvipul294@gmail.com>
2018-02-19 22:55:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('Article one req and served');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('Article two req and served');
